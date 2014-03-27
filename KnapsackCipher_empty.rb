a413bd9ea7e20b976a197cf579891289735d2ded
Soumya Ray <soumya.ray@gmail.com>
2014-03-27 11:14:50 +0800

    Insert comments in KnapsackCipher_empty

--- KnapsackCipher_empty.rb
+++ KnapsackCipher_empty.rb
@@ -2,17 +2,34 @@ require './crypto_math.rb'
 require './modular_arithmetic.rb'
 
 class KnapsackCipher
+	# Default values of knapsacks, primes
 	M = 41
 	N = 491
 	DEF_SUPER = SuperKnapsack.new([2, 3, 7, 14, 30, 57, 120, 251])
 	DEF_GENERAL = DEF_SUPER.to_general(M, N)
 
+	# Returns an encrypted array of integers given a plaintext and general knapsack array
+	# Params:
+	# - plaintext: String object to be encrypted
+	# - generalknap: Array object containing general knapsack numbers
+	# Returns:
+	# - Array of encrypted numbers
 	def self.encrypt(plaintext, generalknap=DEF_GENERAL)
-
+		# TODO: implement this method
 	end
 
+	# Returns an decrypted String given an encrypted Array of numbers, SuperKnapsack, and prime numbers
+	# Params:
+	# - cipherarray: Array of encrypted numbers
+	# - superknap: SuperKnapsack object
+	# - m: prime number
+	# - n: prime number
+	# Returns:
+	# - String of plain text
 	def self.decrypt(cipherarray, superknap=DEF_SUPER, m=M, n=N)
+		raise(ArgumentError, "Argument should a SuperKnapsack object") unless superknap.is_a? SuperKnapsack
 
+		# TODO: implement this method
 	end
 
 end
