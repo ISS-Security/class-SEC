73cbbb9ff56126dda4cbf5491b11e19c74d95699
Soumya Ray <soumya.ray@gmail.com>
2014-03-27 12:00:04 +0800

    Changed indentation to 2 spaces (Ruby style)

--- crypto_math.rb
+++ crypto_math.rb
@@ -3,70 +3,70 @@ require 'Prime'
 # Adds a factorial function to Ruby's Integer object
 # code from: http://rosettacode.org/wiki/Factorial#Ruby
 class Integer
-	def factorial
-		(1..self).reduce(:*) || 1
-	end
+  def factorial
+    (1..self).reduce(:*) || 1
+  end
 
-	# demo method -- perhaps just use .to_s(2) in regular use
-	def to_bi
-		self.to_s(2)
-	end
+  # demo method -- perhaps just use .to_s(2) in regular use
+  def to_bi
+    self.to_s(2)
+  end
 end
 
 class String
-	def binary_op(other)
-		throw :mismatchedStringLengths unless self.length == other.length
-		other_a = other.unpack("U*")
-		self.unpack("U*").map.with_index do |s, i|
-		 	yield s, other_a[i]
-		end.pack("U*")
-	end
+  def binary_op(other)
+    throw :mismatchedStringLengths unless self.length == other.length
+    other_a = other.unpack("U*")
+    self.unpack("U*").map.with_index do |s, i|
+      yield s, other_a[i]
+    end.pack("U*")
+  end
 
-	def ^(other)
-		binary_op(other) {|a, b| a^b}
-	end
+  def ^(other)
+    binary_op(other) {|a, b| a^b}
+  end
 
-	def &(other)
-		binary_op(other) {|a, b| a&b}
-	end
+  def &(other)
+    binary_op(other) {|a, b| a&b}
+  end
 
-	def |(other)
-		binary_op(other) {|a, b| a|b}
-	end
+  def |(other)
+    binary_op(other) {|a, b| a|b}
+  end
 
-	# returns string as an array of ascii numbers in binary
-	def unpack_bi()
-		self.unpack("U*").map {|i| i.to_s(2)}
-	end
+  # returns string as an array of ascii numbers in binary
+  def unpack_bi()
+    self.unpack("U*").map {|i| i.to_s(2)}
+  end
 end
 
 class SuperKnapsack < Array
 
-	def self.array_sum(arr)
-		arr.reduce (:+)
-	end
+  def self.array_sum(arr)
+    arr.reduce (:+)
+  end
 
-	def initialize(arr)
-		arr.each.with_index do |a, i|
-			unless i==0
-				if (a <= self.class.array_sum(arr[0..i-1])) then 
-					raise(ArgumentError, "not superincreasing at index #{i}") 
-				end
-			end
-			self.clear
-			self.concat arr
-		end
-	end
+  def initialize(arr)
+    arr.each.with_index do |a, i|
+      unless i==0
+        if (a <= self.class.array_sum(arr[0..i-1])) then 
+          raise(ArgumentError, "not superincreasing at index #{i}") 
+        end
+      end
+      self.clear
+      self.concat arr
+    end
+  end
 
-	def primes?(m,n)
-		return Prime.prime?(m) && Prime.prime?(n)
-	end
+  def primes?(m,n)
+    return Prime.prime?(m) && Prime.prime?(n)
+  end
 
-	def to_general(m, n)
-		argError = "arguments must both be prime" if (!primes?(m,n)) 
-		argError = "#{n} is smaller than superincreasing knapsack" if n <= self.last
-		raise(ArgumentError, argError) unless argError.nil? 
-		self.map {|a| (a*m)%n }
-	end
+  def to_general(m, n)
+    argError = "arguments must both be prime" if (!primes?(m,n)) 
+    argError = "#{n} is smaller than superincreasing knapsack" if n <= self.last
+    raise(ArgumentError, argError) unless argError.nil? 
+    self.map {|a| (a*m)%n }
+  end
 end
 
--- knaptest.rb
+++ knaptest.rb
@@ -1,11 +1,11 @@
 arr = [2, 3, 7, 14, 30, 57, 120, 251]
 
 def array_sum(arr)
-	arr.reduce (:+)
+  arr.reduce (:+)
 end
 
 arr.each.with_index do |a, i|
-	unless i==0
-		if (a < array_sum(arr[0..i-1])) then puts "problem at #{i}" end
-	end
+  unless i==0
+    if (a < array_sum(arr[0..i-1])) then puts "problem at #{i}" end
+  end
 end
