398df615c15f99a489ece6ad83345a00ae4b3076
Soumya Ray <soumya.ray@gmail.com>
2015-03-28 22:24:48 +0800

    Create hash demo

--- demo_crypto/openssl_test.rb
+++ demo_crypto/openssl_test.rb
@@ -29,10 +29,3 @@ def AES_demo(plaintext)
 
   puts "match: #{plaintext == plain}" #=> true
 end
-
-# code adapted from:
-#  http://ruby-doc.org/stdlib-1.9.3/libdoc/openssl/rdoc/OpenSSL/Digest.html
-def SHA256_demo(plaintext)
-  sha256 = OpenSSL::Digest::SHA256.new
-  digest = sha256.digest(plaintext).unpack("H*")
-end
