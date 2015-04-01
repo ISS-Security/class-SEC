63d4ffba5cb6479fb8ae547de5cb71db3498759e
Soumya Ray <soumya.ray@gmail.com>
2015-04-01 21:24:40 +0800

    rename hash demo method name

--- demo_crypto/hash.rb
+++ demo_crypto/hash.rb
@@ -1,8 +1,6 @@
 require 'openssl'
 
-# code adapted from:
-#  http://ruby-doc.org/stdlib-1.9.3/libdoc/openssl/rdoc/OpenSSL/Digest.html
-def SHA256_demo(plaintext)
+def hash_sha256(plaintext)
   sha256 = OpenSSL::Digest::SHA256.new
   digest = sha256.digest(plaintext).unpack("H*")
 end
