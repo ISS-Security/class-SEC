fe71fb2a3db8d978a7b50642339abaec4198b604
Soumya Ray <soumya.ray@gmail.com>
2016-02-25 19:15:35 +0800

    create unrefined version of crypto_math

---
+++ demo_crypto/crypto_math_unrefined.rb
@@ -0,0 +1,53 @@
+class Fixnum
+  def factorial
+    (1..self).reduce(:*) || 1
+  end
+
+  # demo method -- perhaps just use .to_s(2) in regular use
+  def to_bi
+    self.to_s(2)
+  end
+end
+
+class String
+  def binary_op(other)
+    throw :mismatchedStringLengths unless self.length == other.length
+    other_a = other.unpack("U*")
+    self.unpack("U*").map.with_index do |s, i|
+      yield s, other_a[i]
+    end.pack("U*")
+  end
+
+  def ^(other)
+    binary_op(other) {|a, b| a^b}
+  end
+
+  def &(other)
+    binary_op(other) {|a, b| a&b}
+  end
+
+  def |(other)
+    binary_op(other) {|a, b| a|b}
+  end
+
+  # Returns string as an array of ascii numbers in binary
+  def to_bi_a
+    self.chars.map {|c| c.ord.to_s(2)}
+  end
+
+  # modified from: https://gist.github.com/geoffgarside/747242
+  def word_entropy
+    len = self.chars.count.to_f
+    log2 = Math.log(2)
+
+    counts = self.chars.reduce({}) do |h,c|
+      h[c] = (h[c] || 0) + 1
+      h
+    end
+
+    counts.reduce(0) do |entropy, pair|
+      frequency = (pair[1] / len)
+      entropy = (frequency * (Math.log(frequency) / log2))
+    end.abs
+  end
+end
